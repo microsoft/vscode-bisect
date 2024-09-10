5bf0c96944ad6daf836711e6904d75311b9a1cc3
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-09-10 09:50:51 +0200

    feat - progress bar for download

--- src/fetch.ts
+++ src/fetch.ts
@@ -7,6 +7,8 @@ import { https } from 'follow-redirects';
 import { createWriteStream, promises } from 'fs';
 import { dirname } from 'path';
 import { OutgoingHttpHeaders } from 'http';
+import chalk from 'chalk';
+import ProgressBar from 'progress';
 
 export function jsonGet<T>(url: string, headers?: OutgoingHttpHeaders): Promise<T> {
     return new Promise((resolve, reject) => {
@@ -33,9 +35,23 @@ export async function fileGet(url: string, path: string): Promise<void> {
     // Download
     return new Promise((resolve, reject) => {
         const request = https.get(url, res => {
+            const totalSize = parseInt(res.headers['content-length']!, 10);
+
+            const bar = new ProgressBar(`${chalk.gray('[fetch]')} [:bar] :percent of ${(totalSize / 1024 / 1024).toFixed(2)} MB (:rate/MB/s)`, {
+                complete: '=',
+                incomplete: ' ',
+                width: 30,
+                total: totalSize / (1024 * 1024),
+                clear: true
+            });
+
             const outStream = createWriteStream(path);
             outStream.on('close', () => resolve());
             outStream.on('error', reject);
+
+            res.on('data', chunk => {
+                bar.tick(chunk.length / (1024 * 1024));
+            });
 
             res.on('error', reject);
             res.pipe(outStream);
