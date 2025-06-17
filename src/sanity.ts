b9b8e7c01722fa7808d998609234bb52201e13bc
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-06-17 10:53:20 +0200

    .

--- src/sanity.ts
+++ src/sanity.ts
@@ -3,7 +3,6 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import prompts from "prompts";
 import chalk from "chalk";
 import { IBuild } from "./builds.js";
 import { arch, Flavor, Platform, platform, Quality, Runtime } from "./constants.js";
