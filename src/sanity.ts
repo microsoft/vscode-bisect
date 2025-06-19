81a28f1414a914b8c7edfcaeff044da5ac66ec22
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-06-19 18:55:23 +0200

    polish (#38)

--- src/sanity.ts
+++ src/sanity.ts
@@ -5,8 +5,10 @@
 
 import chalk from "chalk";
 import { IBuild } from "./builds.js";
-import { Arch, arch, Flavor, Platform, platform, Quality, Runtime } from "./constants.js";
-import { bisecter, BisectResponse } from "./bisect.js";
+import { Arch, arch, Flavor, LOGGER, logTroubleshoot, Platform, platform, Quality, Runtime, USER_DATA_FOLDER } from "./constants.js";
+import prompts from "prompts";
+import { cleanUserDataDir } from "./files.js";
+import { launcher } from "./launcher.js";
 
 class Sanity {
 
@@ -57,14 +59,94 @@ class Sanity {
         for (let i = 0; i < buildKinds.length; i++) {
             const build = buildKinds[i];
 
-            const result = await bisecter.tryBuild(build, { isBisecting: true, forceReDownload: false, label: build.label });
-            switch (result) {
-                case BisectResponse.Bad:
-                    console.log(`👉 Please report an issue at ${chalk.green('https://github.com/microsoft/vscode/issues')}\n`);
-                    break;
-                case BisectResponse.Quit:
-                    return;
+            const shouldContinue = await this.tryBuild(build, { forceReDownload: false, label: build.label, isLast: i === buildKinds.length - 1 });
+            if (!shouldContinue) {
+                return;
             }
+        }
+    }
+
+    async tryBuild(build: IBuild, options: { forceReDownload: boolean, label: string, isLast: boolean }): Promise<boolean /* continue */> {
+        try {
+            const instance = await launcher.launch(build, options);
+            if (!instance) {
+                return true;
+            }
+
+            console.log();
+            const response = await prompts([
+                {
+                    type: 'select',
+                    name: 'status',
+                    message: `Running ${chalk.green(options.label)}`,
+                    choices: (() => {
+                        const choices = [
+                            { title: options.isLast ? 'Done' : 'Next', value: 'next' },
+                            { title: 'Retry', value: 'retry' }
+                        ];
+
+                        if (build.runtime === Runtime.DesktopLocal && (build.flavor === Flavor.Default || build.flavor === Flavor.DarwinUniversal)) {
+                            choices.push({ title: 'Retry (fresh user data dir)', value: 'retry-fresh' });
+                        }
+
+                        if (!options.isLast) {
+                            choices.push({ title: 'Quit', value: 'quit' });
+                        }
+
+                        return choices;
+                    })()
+                }
+            ]);
+            console.log();
+
+            await instance.stop();
+
+            if (response.status === 'retry-fresh') {
+                cleanUserDataDir();
+            }
+
+            if (response.status === 'retry' || response.status === 'retry-fresh') {
+                return this.tryBuild(build, { ...options, forceReDownload: false });
+            }
+
+            return response.status === 'next';
+        } catch (error) {
+            LOGGER.log(`${chalk.red('\n[error]')} ${error}\n`);
+
+            console.log();
+            const response = await prompts([
+                {
+                    type: 'select',
+                    name: 'status',
+                    message: `Would you like to restart ${chalk.green(options.label)}?`,
+                    choices: (() => {
+                        const choices = [
+                            { title: 'Yes', value: 'retry' }
+                        ];
+
+                        if (build.runtime === Runtime.DesktopLocal && (build.flavor === Flavor.Default || build.flavor === Flavor.DarwinUniversal)) {
+                            choices.push({ title: 'Yes (fresh user data dir)', value: 'retry-fresh' });
+                        }
+
+                        choices.push({ title: 'No', value: 'no' });
+
+                        return choices;
+                    })()
+                }
+            ]);
+            console.log();
+
+            if (response.status === 'retry-fresh') {
+                cleanUserDataDir();
+            }
+
+            if (response.status === 'retry' || response.status === 'retry-fresh') {
+                return this.tryBuild(build, { ...options, forceReDownload: true });
+            }
+
+            logTroubleshoot();
+
+            return true;
         }
     }
 
